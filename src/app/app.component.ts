218917132dd5d291ac79de089091c28e26f19939
Dustin Bunta <dmbunta@gmail.com>
2022-10-11 10:10:34 -0400

    message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'i18n';
+
+  date = Date.now();
+  text = $localize `when in code, must use backticks`;
+
+
+  pluralMapping = { 'schmutz': {'=0': 'no schmutz', '=1': 'one schmutz', 'other': '# Schmutzes'}};
+
+
+
+  numberOfSchmutz = 0;
+
+  oneSchmutz = 1;
+  noSchmutz = 0;
+  allTheSchmutz = 15;
 }
+
+
